98a08f95a30ca41389a120046b3a900ee9d446ca
Ross Stenersen <rossiam@gmail.com>
2025-09-15 11:07:10 -0500

    print debug

--- bin/build.mjs
+++ bin/build.mjs
@@ -13,10 +13,10 @@ const version = process.version.substring(1)
 
 const target = `${os}-${arch}-${version}`
 
-// console.log(`building ${version}`)
-// console.log(`process.arch = [${process.arch}]`)
-// console.log(`process.platform = [${process.platform}]`)
-// console.log(`process.title = [${process.title}]`)
+console.log(`building ${version}`)
+console.log(`process.arch = [${process.arch}]`)
+console.log(`process.platform = [${process.platform}]`)
+console.log(`target = ${target}`)
 
 // TODO: check if already built like nexe_builds does
 if (false) {
